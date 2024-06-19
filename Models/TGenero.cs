44aecc1c154dd2cd0520c25054027483753f4556
Jose Solano <joseandressolano2003@gmail.com>
2024-06-19 10:20:19 -0500

    Proyecto Gestion Cafeteria

---
+++ Models/TGenero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gestion_Cafeteria.Models;
+
+public partial class TGenero
+{
+    public int Id { get; set; }
+
+    public string? Genero { get; set; }
+
+    public virtual ICollection<TTrabajadore> TTrabajadores { get; set; } = new List<TTrabajadore>();
+}
---
+++ Models/TRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gestion_Cafeteria.Models;
+
+public partial class TRole
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public virtual ICollection<TTrabajadore> TTrabajadores { get; set; } = new List<TTrabajadore>();
+}
